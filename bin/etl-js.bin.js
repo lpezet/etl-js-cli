8152f3c71d8b1681443b42751883b9aa2c7ccc95
Luke Pezet <lpezet@gmail.com>
2019-04-11 21:53:58 -0600

    Cleanup. Fixes and tests added.

--- bin/etl-js.bin.js
+++ bin/etl-js.bin.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
-var cli = require('../lib/cli');
-cli.init(process.argv);
+
+const Fs = require('fs');
+const { yamlParse } = require('yaml-cfn');
+const path = require('path');
+const MainClass = require('../lib/main');
+
+var oSettingsFile = path.resolve( process.cwd(), "settings.yml" );
+var oSettings = {};
+if ( Fs.existsSync( oSettingsFile ) ) {
+	oSettings = yamlParse( Fs.readFileSync( oSettingsFile, {encoding: 'utf8'}) );
+}
+var CLIClass = require('../lib/cli');
+new CLIClass( MainClass ).run(oSettings, process.argv);
