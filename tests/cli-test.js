c73bdfe06c757122e4970ac9aac4beca93fa04c2
Luke Pezet <lpezet@gmail.com>
2019-08-12 23:14:43 -0600

    Tests added.

--- tests/cli-test.js
+++ tests/cli-test.js
@@ -37,6 +37,40 @@ describe('cli',function(){
 		
 	});
 	
+	it('initRejected', function(done) {
+		var oMainClass = function() {
+			return {
+				init: function() {
+					return Promise.reject(new Error("Test error"));
+				}
+			}
+		}
+		var oTested = new TestedClass( oMainClass );
+		oTested.run({}, [ '/usr/local/bin/node', 'etl-js', 'init']).then( function() {
+			done('Exepected error');
+		}, function( pError ) {
+			done();
+		});
+		
+	});
+	
+	it('initWithError', function(done) {
+		var oMainClass = function() {
+			return {
+				init: function() {
+					throw new Error("Test error");
+				}
+			}
+		}
+		var oTested = new TestedClass( oMainClass );
+		oTested.run({}, [ '/usr/local/bin/node', 'etl-js', 'init']).then( function() {
+			done('Exepected error');
+		}, function( pError ) {
+			done();
+		});
+		
+	});
+	
 	it('run', function(done) {
 		var oRunCalled = false;
 		var oMainClass = function() {
@@ -50,6 +84,92 @@ describe('cli',function(){
 		var oETLFile = path.resolve(__dirname, 'sample_etl.yml');
 		var oTested = new TestedClass( oMainClass );
 		oTested.run({}, [ '/usr/local/bin/node', 'etl-js', 'run', oETLFile]).then( function() {
+			try {
+				assert.isTrue( oRunCalled );
+				done();
+			} catch( e ) {
+				done( e );
+			}
+		}, function( pError ) {
+			done( pError );
+		});
+		
+	});
+	
+	it('runRejected', function(done) {
+		var oMainClass = function() {
+			return {
+				run: function() {
+					return Promise.reject( new Error("Test error") );
+				}
+			}
+		}
+		var oETLFile = path.resolve(__dirname, 'sample_etl.yml');
+		var oTested = new TestedClass( oMainClass );
+		oTested.run({}, [ '/usr/local/bin/node', 'etl-js', 'run', oETLFile]).then( function() {
+			done('Expected error.');
+		}, function( pError ) {
+			done();
+		});
+		
+	});
+	
+	it('runWithError', function(done) {
+		var oMainClass = function() {
+			return {
+				run: function() {
+					throw new Error("Test error");
+				}
+			}
+		}
+		var oETLFile = path.resolve(__dirname, 'sample_etl.yml');
+		var oTested = new TestedClass( oMainClass );
+		oTested.run({}, [ '/usr/local/bin/node', 'etl-js', 'run', oETLFile]).then( function() {
+			done('Expected error.');
+		}, function( pError ) {
+			done();
+		});
+		
+	});
+	
+	it('runETLSet', function(done) {
+		var oRunCalled = false;
+		var oMainClass = function() {
+			return {
+				run: function() {
+					oRunCalled = true;
+					return Promise.resolve();
+				}
+			}
+		}
+		var oETLFile = path.resolve(__dirname, 'sample_etl.yml');
+		var oTested = new TestedClass( oMainClass );
+		oTested.run({}, [ '/usr/local/bin/node', 'etl-js', 'run', oETLFile, 'mySet']).then( function() {
+			try {
+				assert.isTrue( oRunCalled );
+				done();
+			} catch( e ) {
+				done( e );
+			}
+		}, function( pError ) {
+			done( pError );
+		});
+		
+	});
+	
+	it('runSilent', function(done) {
+		var oRunCalled = false;
+		var oMainClass = function() {
+			return {
+				run: function() {
+					oRunCalled = true;
+					return Promise.resolve();
+				}
+			}
+		}
+		var oETLFile = path.resolve(__dirname, 'sample_etl.yml');
+		var oTested = new TestedClass( oMainClass );
+		oTested.run({}, [ '/usr/local/bin/node', 'etl-js', '-s', 'run', oETLFile]).then( function() {
 			try {
 				assert.isTrue( oRunCalled );
 				done();
--- tests/main-test.js
+++ tests/main-test.js
@@ -20,17 +20,44 @@ describe('main',function(){
 	const SETTINGS_FILE = path.resolve( process.cwd(), "settings.yml" );
 	const ETL_FILE = path.resolve( process.cwd(), "etl.yml" );
 	
-	
 	before(function(done) {
 		done();
 	});
 	
-	after(function(done) {
+	afterEach(function(done) {
 		if ( Fs.existsSync( SETTINGS_FILE ) ) Fs.unlinkSync( SETTINGS_FILE );
 		if ( Fs.existsSync( ETL_FILE ) ) Fs.unlinkSync( ETL_FILE );
 		//if ( Fs.existsSync( 'test-etl-js.log' ) ) Fs.unlinkSync( 'test-etl-js.log' );
-		
 		done();
+	});
+	
+	it('initFilesAlreadyExist', function(done) {
+		var oTested = new TestedClass( logger );
+		oTested.init( {} ).then( function() {
+			oTested.init( {} ).then( function() {
+				done('Expected error (file already exists');
+			}, function( pError ) {
+				// good
+				done();
+			});
+		}, function( pError ) {
+			done( pError );
+		});
+		
+	});
+	
+	it('initFilesAlreadyExistForce', function(done) {
+		var oTested = new TestedClass( logger );
+		oTested.init( {} ).then( function() {
+			oTested.init( { "force": true } ).then( function() {
+				done();
+			}, function( pError ) {
+				done( pError );
+			});
+		}, function( pError ) {
+			done( pError );
+		});
+		
 	});
 	
 	it('init', function(done) {
@@ -50,17 +77,68 @@ describe('main',function(){
 		
 	});
 	
-	it('run', function(done) {
+	it('runLocal', function(done) {
 		var oSettings = {etl: { executor: 'local1' }, executors: { local1: { type: 'local' } }};
 		var oETLActivities = {etl: {}};
 		var oParameters = {};
 		var oTested = new TestedClass( logger );
-		oTested.run( oSettings, oETLActivities, oParameters).then( function() {
-			done();
+		oTested.init( {} ).then( function() {
+			oTested.run( oSettings, oETLActivities, oParameters).then( function() {
+				done();
+			}, function( pError ) {
+				done( pError );
+			});
 		}, function( pError ) {
 			done( pError );
 		});
 		
 	});
+	
+	it('runRemote', function(done) {
+		var oSettings = {etl: { executor: 'remote1' }, executors: { remote1: { type: 'remote' } }};
+		var oETLActivities = {etl: {}};
+		var oParameters = {};
+		var oTested = new TestedClass( logger );
+		oTested.init( {} ).then( function() {
+			oTested.run( oSettings, oETLActivities, oParameters).then( function() {
+				done();
+			}, function( pError ) {
+				done( pError );
+			});
+		}, function( pError ) {
+			done( pError );
+		});
+		
+	});
+	
+	it('runUnknowExecutorType', function(done) {
+		var oSettings = {etl: { executor: 'unknown1' }, executors: { unknown1: { type: 'unknown' } }};
+		var oETLActivities = {etl: {}};
+		var oParameters = {};
+		var oTested = new TestedClass( logger );
+		oTested.init( {} ).then( function() {
+			oTested.run( oSettings, oETLActivities, oParameters).then( function() {
+				done( 'Expected error.');
+			}, function( pError ) {
+				done();
+			});
+		}, function( pError ) {
+			done( pError );
+		});
+		
+	});
+	
+	it('invalidSettings', function(done) {
+		var oInvalidSettings = {};
+		var oETLActivities = {etl: {}};
+		var oParameters = {};
+		var oTested = new TestedClass( logger );
+		oTested.run( oInvalidSettings, oETLActivities, oParameters).then( function() {
+			done( 'Expected error.');
+		}, function( pError ) {
+			done();
+		});
+	});
+	
 	
 });
